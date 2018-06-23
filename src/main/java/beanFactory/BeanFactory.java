4c3325db1e7e442d2b58a10cbd9bd792848087c1
notebook_idea <2531389274@qq.com>
2018-06-23 22:20:55 +0800

    新增beanFactory构造器方法

---
+++ src/main/java/beanFactory/BeanFactory.java
@@ -0,0 +1,15 @@
+package beanFactory;/**
+ * Created by Administrator on 2018-6-23 0023.
+ */
+
+/**
+ * user is lwb
+ **/
+
+
+public class BeanFactory {
+    public void beanFactory(){
+        System.out.println("factoryBean");
+        return ;
+    }
+}
